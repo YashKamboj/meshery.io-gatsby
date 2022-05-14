6d0f736af9e1ab6db797e6a4cd8bdc0f117da717
Yash Kamboj <yashkamboj29@gmail.com>
2022-05-14 15:02:56 +0530

    made community card data dynamic

--- src/components/Projects-home/index.js
+++ src/components/Projects-home/index.js
@@ -3,31 +3,44 @@ import ProjectItemWrapper from "./projectSection.style";
 
 import projectImage3 from "../../assets/images/meshery-logo-light.png";
 
+const data = [
+  {
+    name:"Community Calendar",
+     description: "Join the weekly meetings by subscribing to our community calendar",
+     link: "https://bit.ly/2SbrRhe",
+  },
+  {
+    name:"Community Meetings",
+     description: "Watch community meeting recordings on our YouTube channel",
+     link: "https://www.youtube.com/c/Layer5io?sub_confirmation=1",
+  },
+  {
+    name:"Join the Conversation",
+     description: "Ask questions, find answers and share knowledge on our Discussion Forum",
+     link: "https://discuss.layer5.io",
+  },
+  {
+    name:"Community Guide",
+     description: "Read the Welcome Guide to familiarize yourself with the community",
+     link: "https://docs.google.com/document/d/17OPtDE_rdnPQxmk2Kauhm3GwXF1R5dZ3Cj8qZLKdo5E/edite",
+  }
+]
+
 
 const Projects = () => {
   return (
     <ProjectItemWrapper id="projects">
+      
       <div className="callout">
-        <a className="card" href="https://bit.ly/2SbrRhe" target="_blank" rel="noreferrer">
-          <h3>Community Calendar</h3>
-          <p className="small">Join the weekly meetings by subscribing to our community calendar</p>
-          <img className="go-corner" src={projectImage3} alt="projectImage" href="https://bit.ly/2SbrRhe" />
+      {data.map((data) =>{
+        return(
+        <a className="card" href={data.link} target="_blank" rel="noreferrer">
+          <h3>{data.name}</h3>
+          <p className="small">{data.description}</p>
+          <img className="go-corner" src={projectImage3} alt="projectImage" href={data.link} />
         </a>
-        <a className="card" href="https://www.youtube.com/c/Layer5io?sub_confirmation=1" target="_blank" rel="noreferrer">
-          <h3>Community Meetings</h3>
-          <p className="small">Watch community meeting recordings on our YouTube channel</p>
-          <img className="go-corner" src={projectImage3} alt="projectImage" href="https://www.youtube.com/channel/UCFL1af7_wdnhHXL1InzaMvA" />
-        </a>
-        <a className="card" href="https://discuss.layer5.io" target="_blank" rel="noreferrer">
-          <h3>Join the Conversation</h3>
-          <p className="small">Ask questions, find answers and share knowledge on our Discussion Forum</p>
-          <img className="go-corner" src={projectImage3} alt="projectImage" href="https://discuss.layer5.io" />
-        </a>
-        <a className="card" href="https://docs.google.com/document/d/17OPtDE_rdnPQxmk2Kauhm3GwXF1R5dZ3Cj8qZLKdo5E/edit" target="_blank" rel="noreferrer">
-          <h3>Community Guide</h3>
-          <p className="small">Read the Welcome Guide to familiarize yourself with the community</p>
-          <img className="go-corner" src={projectImage3} alt="projectImage" href="https://docs.google.com/document/d/17OPtDE_rdnPQxmk2Kauhm3GwXF1R5dZ3Cj8qZLKdo5E/edit" />
-        </a>
+        );
+})}
       </div>
 
     </ProjectItemWrapper>
