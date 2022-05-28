1af548eb6d92e4f301ca3ead084ddcbfc5da812d
Yash Kamboj <yashkamboj29@gmail.com>
2022-05-29 00:05:00 +0530

    added filters and modal to catalog page

--- src/components/Catalog/cards/catalog_card.js
+++ src/components/Catalog/cards/catalog_card.js
@@ -1,18 +1,76 @@
-import React from "react";
+import React, {useState} from "react";
 import patternslogo from "../../../assets/images/service-mesh-pattern.svg";
-// import patternsdata from "./patterns.data"
+import ReactModal from "react-modal";
 import CardWrapper from "./card.styles";
 
 const Catalogcard = ({ pattern }) => {
+
+  const customStyles = {
+    content: {
+      top: '50%',
+      left: '50%',
+      right: 'auto',
+      bottom: 'auto',
+      // marginRight: '-50%',
+      transform: 'translate(-50%, -50%)',
+    },
+  };
+
+  const [modalIsOpen,setIsOpen] = useState(false);
+  
+  const openModal = () => setIsOpen(true);
+
+  const closeModal = () => setIsOpen(false);
   return (
     <CardWrapper>
-      {/* <div className="link open-modal-btn"> */}
-      <div className="card">
+      <div className="card" onClick={openModal}  >
         <div className="chip">
           <small className="pattern-type"> {pattern.filters.type}</small>
         </div>
+
+        <h4 className="pattern-name">{pattern.name}</h4>
+        <img className="pattern-image" src={pattern.image} />
+        <div>
+          <h6 className="pattern-id">
+            <img className="smp" src={patternslogo} loading="lazy" />
+            {pattern.patternId}
+          </h6>
+        </div>
       </div>
+
+      <ReactModal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        ariaHideApp={false}
+        style={customStyles}
+        contentLabel="Contact Form"
+      >
+      <div id="open-modal" className="modal-window">
+        <div>
+          <button className="modal-close close" style={{right:"0", position: "fixed"}} onClick={closeModal}>close</button>
+          <div className="chip ">
+            <h6 className="pattern-type"> {pattern.filters.type}</h6>
+          </div>
+          <div style={{display: "flex", gap:"1rem"}}>
+          <p /><div className="modal-image-container"><div className="modal-image"><img style={{width: "50rem"}} src={pattern.image}/> <h4 className="related-patterns">Related Patterns</h4>
+              {/* MINICARD-START */}
+              {/* {'{'}% include related-patterns.html %{'}'} */}
+            </div>
+          </div>
+          {/* MINICARD-END */}
+          <div className="test" style={{marginTop:"-4rem"}}>
+          <h3 style={{textAlign: 'center'}}>{pattern.name}</h3>
+          <h4>What this pattern does:</h4><p>{pattern.patternInfo}</p>
+          <h4>Caveats and Consideration: </h4><p>{pattern.patternCaveats}</p><p />
+          <h4>Compatibilty:</h4>
+          {/* {'{'}% include partials/compatibility.html%{'}'}
+          {'{'}% include copy-and-download.html %{'}'} */}
+          </div>
+        </div></div></div>
+              </ReactModal>
+              
     </CardWrapper>
+
   );
 };
 
--- src/components/Catalog/filters/index.js
+++ src/components/Catalog/filters/index.js
@@ -1,16 +1,59 @@
 import React from 'react'
+import filtersoption from "./filter.options"
+import FilterWrapper from "./filters.style"
 
-const filters = ({filter}) => {
+const Filters = (props) => {
+    // const data = React.useMemo(() => Filtersoption);
+let typeOptions = filtersoption.filter((filtersoption) => filtersoption.category === "Categories");
+let productOptions = filtersoption.filter((filtersoption) => filtersoption.category === "Compatibilty");
+let meshOptions = filtersoption.filter((filtersoption) => filtersoption.category === "Technology");
   return (
-      {filter.map((filter) => {
+    <FilterWrapper>
+      
+      <p><strong>Categories</strong></p>
+    <div className="list">
+    <ul className="ul-open">
+      {typeOptions.map((typeOptions) => {
+        return typeOptions.subdata.map((x) =>{
           return(
-    <div>
-        <h3>{filter.name}</h3>
-        <p>{filter.value}</p>
+        <li key={x.id}>
+          <label>
+            <input type="checkbox"  value={x.value}  onChange={props.handleChange} />
+            <span> {x.label}</span>
+            </label>
+            </li>
+          )})          
+})}
+      </ul>
+      </div>
+      <p><strong>Compatibilty</strong></p>
+    <div className="list">
+      <ul className= "ul-open">
+      {productOptions[0].subdata.map((x) => (
+          <li key={x.id}>
+            <label>
+              <input type="checkbox"  value={x.value}  onChange={props.handleChange} />
+              <span> {x.label}</span>
+            </label> 
+          </li>
+        ))}
+      </ul>
     </div>
-          )
-      })}
+    <p><strong>Technology:</strong></p>
+    <div className="list">
+      <ul className= "ul-open">
+        {meshOptions[0].subdata.map((x) => (
+          <li key={x.id}>
+            <label>
+              <input type="checkbox"  value={x.value}   onChange={props.handleChange} />
+              <span> {x.label}</span>
+            </label> 
+          </li>
+        ))}
+      </ul>
+    </div> 
+    </FilterWrapper>
   )
 }
 
-export default filters
+export default Filters
