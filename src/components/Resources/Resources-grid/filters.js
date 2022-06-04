6011eff2ee9005e086a54506c98b728c56d1674c
Yash Kamboj <yashkamboj29@gmail.com>
2022-06-04 22:21:14 +0530

    fixed bugs

--- src/components/Resources/Resources-grid/filters.js
+++ src/components/Resources/Resources-grid/filters.js
@@ -7,18 +7,10 @@ import ResourceNavigationWrapper from "./filters.style";
 const Navigation = (props) => {
 
   const data = React.useMemo(() => options);
-  let typeOptions = data.filter((data) => data.category === "Type");
-  let productOptions = data.filter((data) => data.category === "Product");
+  let typeOptions = data.filter((data) => data.category === "Categories");
+  let productOptions = data.filter((data) => data.category === "Compatibilty");
   let techOptions = data.filter((data) => data.category === "Technology");
-  let meshOptions = data.filter((data) => data.category === "Service Mesh");
   let i=0;
-  
-  const typesFieldValues = types.map(type => type.fieldValue);
-
-  const productsFieldValues = products.map(product => product.fieldValue);
-  const technologiesFieldValues = technologies.map(tech => tech.fieldValue);
-  const meshesFieldValues = meshes.map(mesh => mesh.fieldValue);
-
 
 
   return (
@@ -34,10 +26,7 @@ const Navigation = (props) => {
                 <label>
                   <input type="checkbox"  value={x.value} onChange={props.handleChange} />
                   <span> {x.label}</span>
-                  { types && types.map(type => (
-                    type.fieldValue === x.value 
-                  ))}
-                  { typesFieldValues && !typesFieldValues.includes(x.value)}
+        
                 </label> 
               </li>
             ))}
@@ -51,10 +40,7 @@ const Navigation = (props) => {
                 <label>
                   <input type="checkbox"  value={x.value} onChange={props.handleChange} />
                   <span> {x.label}</span>
-                  { products && products.map(product => (
-                    product.fieldValue === x.value
-                  ))}
-                  { productsFieldValues && !productsFieldValues.includes(x.value)}
+  
                 </label> 
               </li>
             ))}
@@ -68,28 +54,6 @@ const Navigation = (props) => {
                 <label>
                   <input type="checkbox"  value={x.value} onChange={props.handleChange} />
                   <span> {x.label}</span>
-                  { technologies && technologies.map(tech => (
-                    tech.fieldValue === x.value 
-                  ))}
-                  { technologiesFieldValues && !technologiesFieldValues.includes(x.value)}
-
-                </label> 
-              </li>
-            ))}
-          </ul>
-        </div>
-
-        <div className="list">
-          <ul>
-            {meshOptions[0].subdata.map((x) => (
-              <li key={x.id}>
-                <label>
-                  <input type="checkbox"  value={x.value} onChange={props.handleChange} />
-                  <span> {x.label}</span>
-                  { meshes && meshes.map(mesh => (
-                    mesh.fieldValue === x.value
-                  ))}
-                  { meshesFieldValues && !meshesFieldValues.includes(x.value) }
 
                 </label> 
               </li>
--- src/components/Resources/Resources-grid/options.js
+++ src/components/Resources/Resources-grid/options.js
@@ -1,47 +1,38 @@
 export const options =  [
   {
-    category: "Type",
+    category: "Categories",
     subdata: [
-      { id: 0, value: "Article", label: "Article" },
-      { id: 1, value: "Blog", label: "Blog" },
-      { id: 2, value: "News", label: "News" },
-      { id: 3, value: "Workshop", label: "Workshop" },
-      { id: 4, value: "Recorded Webinar", label: "Recorded Webinar" },
-      { id: 5, value: "Event", label: "Event" },
-      { id: 6, value: "Case Study", label: "Case Study" },
-      { id: 7, value: "Demo", label: "Demo" },
-      { id: 8, value: "Guide", label: "Guide" },
-      { id: 9, value: "FAQ", label: "FAQ" },
-      { id: 10, value: "Interview", label: "Interview" },
-      { id: 11, value: "Tutorial", label: "Tutorial" }]
+      { id: 0, value: "All", label: "All" },
+      { id: 1, value: "Deployment", label: "Deployment" },
+      { id: 2, value: "Traffic Management", label: "Traffic Management" },
+      { id: 3, value: "Security", label: "Security" },
+      { id: 4, value: "Workloads", label: "Workloads" },
+      { id: 5, value: "Observability", label: "Observability" },
+      { id: 6, value: "Troubleshooting", label: "Troubleshooting" },
+      { id: 7, value: "Scaling", label: "Scaling" },
+      { id: 8, value: "Resiliency", label: "Resiliency" },
+    ]
   },
   {
-    category: "Product",
+    category: "Compatibilty",
     subdata: [
-      { id: 0, value: "Meshery", label: "Meshery" },
-      { id: 1, value: "Service Mesh Performance", label: "SMP" },
-      { id: 2, value: "Nighthawk", label: "Nighthawk" }]
+      { id: 0, value: "Istio", label: "Istio" },
+      { id: 1, value: "Linkerd", label: "Linkerd" },
+      { id: 2, value: "Consul", label: "Consul" },
+      { id: 3, value: "Kuma", label: "Kuma" },
+      { id: 4, value: "App Mesh", label: "App Mesh" },
+      { id: 5, value: "OSM", label: "OSM" },
+      { id: 6, value: "Traefik", label: "Traefik" },
+      { id: 7, value: "Nginx", label: "Nginx" },
+      { id: 8, value: "NSM", label: "NSM" },
+    ]
   },
   {
     category: "Technology",
     subdata: [
-      { id: 0, value: "API", label: "API" },
-      { id: 1, value: "Cloud", label: "Cloud" },
-      { id: 2, value: "Docker", label: "Docker" },
-      { id: 3, value: "JWT", label: "JWT" },
-      { id: 4, value: "Kubernetes", label: "Kubernetes" },
-      { id: 5, value: "WebAssembly", label: "WebAssembly" }]
-  },
-  {
-    category: "Service Mesh",
-    subdata: [
-      { id: 0, value: "Consul", label: "Consul" },
-      { id: 1, value: "Linkerd", label: "Linkerd" },
-      { id: 2, value: "Istio", label: "Istio" },
-      { id: 3, value: "Open Service Mesh", label: "Open Service Mesh" },
-      { id: 4, value: "Kuma", label: "Kuma" },
-      { id: 5, value: "Network Service Mesh", label: "Network Service Mesh" },
-      { id: 6, value: "Traefik Mesh", label: "Traefik Mesh" }]
+      { id: 0, value: "WebAssembly", label: "WebAssembly" },
+      { id: 1, value: "JWT", label: "JWT" },
+    ]
   },
 ];
   
