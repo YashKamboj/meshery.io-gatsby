b5320b090da2c42853ee3e47b0324dca29d3d78b
Yash Kamboj <yashkamboj29@gmail.com>
2022-07-06 01:18:02 +0530

    fixed errors
    Signed-off-by: Yash Kamboj <yashkamboj29@gmail.com>

--- src/components/Catalog/index.js
+++ src/components/Catalog/index.js
@@ -1,99 +1,37 @@
-import React, {useState} from "react";
-import  CardWrapper  from "./Card.style";
-import ReactModal from "react-modal";
-import {AiOutlineClose} from '@react-icons/all-files/ai/AiOutlineClose'
-import { copyToClipboard } from "../../CodeBlock/copy-to-clipboard"
-import Button from "../../../reusecore/Button"
+import React from "react";
+import Card from "./Card";
+import  CommingSoonCard from "./Card/commingSoon_Card"
+import { Row, Col } from "../../reusecore/Layout";
 
+import { GridWrapper } from "./Grid.style";
 
-const Card = ({ data }) => {
-
-  const [modalIsOpen,setIsOpen] = useState(false);
-
-const openModal = () => setIsOpen(true);
-
-const closeModal = () => setIsOpen(false);  
-
-const [copyText, setCopyText] = useState("Copy");
-
-const handleClick = () => {
-  copyToClipboard(data.URL)
-  setCopyText("Copied!");
-  setTimeout(() => setCopyText("Copy"), 5000);
-}
-
-  const customStyles = {
-    content: {
-      top: '25rem',
-      left: '50%',
-      right: 'auto',
-      bottom: 'auto',
-      transform: 'translate(-50%, -50%)',
-      overflow: "auto",
-      overflowX: "hidden",
-      height: "35rem",
-      borderRadius: "1rem", 
-      width: "60%",
-    },
-  };
-
+const CatalogGrid = (props) => {
 
   return (
-    <CardWrapper >
-      <div className="card" onClick={openModal}>
-      <div className="chip">
-          <small className="pattern-type"> {data.type}</small>
-        </div>
-          <h4 className="pattern-name">
-            {data.name}
-          </h4>
-          <img
-            src = {data.image}
-            imgStyle={{ objectFit: "contain" }}
-            alt={data.name}
-            className="pattern-image"
-          />
+    <GridWrapper>
+      <div className="resource-grid-wrapper">
+        <Row>
+          {props.data.map(( data ) => {
+             if(data.Status !== "ComingSoon") {
+           return ( <Col  xs={12} sm={6} xl={4}>   
+             <Card data={data} />
+            </Col>
+           )
+             }
 
-          <h6 className="pattern-id">  {data.Id}</h6>
-          </div>
-
-          <ReactModal
-        isOpen={modalIsOpen}
-        onRequestClose={closeModal}
-        ariaHideApp={false}
-        style={customStyles}
-        contentLabel="Contact Form"
-      >
-      <div id="open-modal" className="modal-window">
-        <div>
-          <AiOutlineClose  style={{right:"1rem",top: "1rem", position: "fixed", cursor: "pointer"}} onClick={closeModal}  />
-          <div className="chip ">
-            <h6 className="pattern-type"> {data.type}</h6>
-          </div>
-          <div style={{display: "flex", gap:"1rem"}}>
-          <p /><div className="modal-image-container"><div className="modal-image"><img style={{width: "50rem"}} src={data.image}/> <h4 className="related-patterns">Related Patterns</h4>
-              {/* MINICARD-START */}
-              {/* {'{'}% include related-patterns.html %{'}'} */}
-            </div>
-          </div>
-          {/* MINICARD-END */}
-          <div className="test" style={{marginTop:"-4rem"}}>
-          <h3 style={{textAlign: 'center'}}>{data.name}</h3>
-          <h4>What this pattern does:</h4><p>{data.patternInfo}</p>
-          <h4>Caveats and Consideration: </h4><p>{data.patternCaveats}</p><p />
-          <h4>Compatibilty:</h4>
-          <img style={{width:"2rem"}} src={data.compatibilityIcon} />
-          <div style={{marginTop: "0.5rem"}}>
-          <Button secondary className="banner-btn two" title={copyText}  onClick={handleClick} />
-          <a style={{textDecoration: "none"}} href={'/assets/catalogdata/'+ data.downloadLink } download>  <Button secondary className="banner-btn two" title="Click to download" /></a>
-          </div>
-          </div>
-        </div></div></div>
-              </ReactModal>
-
-      
-    </CardWrapper>
+            })}
+         {props.data.map(( data ) => {
+           if (data.Status === "ComingSoon") {
+              return ( <Col  xs={12} sm={6} xl={4}>   
+                <CommingSoonCard data={data} />
+               </Col>
+              )
+             }
+            })}
+        </Row> 
+      </div>
+     </GridWrapper>
   );
 };
 
-export default Card;
+export default CatalogGrid;
